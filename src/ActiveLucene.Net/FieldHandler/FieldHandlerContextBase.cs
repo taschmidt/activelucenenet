f36b9a39c0581feb029a7993980c7b70805a2378
tim@schmidthole.com <tim@schmidthole.com@997331cc-fb45-5a07-65df-c6c94e6600ff>
2010-04-10 19:32:58 +0000

    Forgot a file
    git-svn-id: https://activelucenenet.googlecode.com/svn/trunk@26 997331cc-fb45-5a07-65df-c6c94e6600ff

---
+++ src/ActiveLucene.Net/FieldHandler/FieldHandlerContextBase.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2010 Tim Schmidt and Kevin Dotzenrod
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Lucene.Net.Documents;
+
+namespace ActiveLucene.Net.FieldHandler
+{
+    public abstract class FieldHandlerContextBase<T> : IFieldHandlerContext<T>
+    {
+        protected Field Field;
+
+        public abstract T StringToValue(string value);
+        public abstract string ValueToString(T value);
+
+        public FieldHandlerConfiguration Configuration { get; private set; }
+
+        public void Init(FieldHandlerConfiguration configuration)
+        {
+            Configuration = configuration;
+            Field = new Field(Configuration.Name, "", Configuration.Store, Configuration.Index);
+        }
+
+        public T GetValue(Document document)
+        {
+            return IfNotNull(document.Get(Configuration.Name), StringToValue);
+        }
+
+        public void SetFields(Document document, T value)
+        {
+            document.Add(Field.Set(ValueToString(value)));
+        }
+
+        protected T IfNotNull(string str, Func<string, T> stringToValue)
+        {
+            return !String.IsNullOrEmpty(str) ? stringToValue(str) : default(T);
+        }
+    }
+}
