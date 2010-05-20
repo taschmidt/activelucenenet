7a09f6889c4ae06033f5411e0422f5cdda71e398
tim@schmidthole.com <tim@schmidthole.com@997331cc-fb45-5a07-65df-c6c94e6600ff>
2010-05-20 18:42:47 +0000

    Fixing build numbering
    git-svn-id: https://activelucenenet.googlecode.com/svn/trunk@50 997331cc-fb45-5a07-65df-c6c94e6600ff

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("ActiveLucene.Net")]
 [assembly: AssemblyCopyrightAttribute("Copyright ©  2010")]
-[assembly: AssemblyVersionAttribute("0.1.107.5520")]
-[assembly: AssemblyFileVersionAttribute("0.1.107.5520")]
+[assembly: AssemblyVersionAttribute("0.1.120.4934")]
+[assembly: AssemblyFileVersionAttribute("0.1.120.4934")]
 [assembly: AssemblyInformationalVersionAttribute("0.1")]
 
